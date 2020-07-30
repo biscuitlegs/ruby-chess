dc3c0bd99808c652f679d6beff2ca8f89648e5d8
Bradley <bmcguigandev@outlook.com>
2020-07-30 11:52:25 +0100

    add square class and tests

--- lib/chess.rb
+++ lib/chess.rb
@@ -5,3 +5,15 @@ class Board
         @squares = Array.new(8) { Array.new(8) { square ? square : Square.new } }
     end
 end
+
+class Square
+    attr_reader :piece
+
+    def initialize(piece=nil)
+        @piece = piece
+    end
+
+    def occupied?
+        self.piece ? true : false
+    end
+end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -10,3 +10,33 @@ describe "Board" do
         end
     end
 end
+
+describe "Square" do
+    let (:queen) { double("queen") }
+
+    describe "#initialize" do
+        context "when no piece is given" do
+            it "starts with no piece" do
+                expect(Square.new.piece).to eql(nil)
+            end
+        end
+        context "when a piece is given" do
+            it "starts with the given piece" do
+                expect(Square.new(queen).piece).to eql(queen)
+            end
+        end
+    end
+
+    describe "#occupied?" do
+        context "when the square has no piece" do
+            it "returns false" do
+                expect(Square.new.occupied?).to eql(false)
+            end
+        end
+        context "when the square has a piece" do
+            it "returns true" do
+                expect(Square.new(queen).occupied?).to eql(true)
+            end
+        end
+    end
+end

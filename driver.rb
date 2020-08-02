02cc01667fd077b1deaa496afc08bd0de455e0b7
Bradley <bmcguigandev@outlook.com>
2020-08-02 20:22:15 +0100

    add method to get legal diagonal moves

--- driver.rb
+++ driver.rb
@@ -3,4 +3,8 @@ require_relative "lib/chess.rb"
 board = Board.new
 #board.populate
 board.place_piece("Rook", "e5")
+board.place_piece("Pawn", "f6")
+board.place_piece("Queen", "g7")
+board.place_piece("Bishop", "h8")
 board.show
+p board.get_diagonal_moves("a1")
